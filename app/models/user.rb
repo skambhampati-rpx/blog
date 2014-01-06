af437227bb322f52d4b131c7d1a76aa056040744
skambhampati <skambhampati@rpxcorp.com>
2014-01-06 18:35:55 +0530

    Adding rspec code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ActiveRecord::Base
 has_many :posts
 
+has_many :orders
 
+has_many :active_orders, :class_name => "Order",   :conditions => ["where orderd_date >= ?", Time.now]
+before_save :get_orders_count
 
+private
 
+def get_orders_count
+  if self.orders.count > 2
+    raise "User can place only two orders"
+  end
+end
 
 end
